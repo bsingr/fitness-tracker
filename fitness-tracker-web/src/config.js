4ecf02eef82edd4d1169423ea7973a9980c39fb0
Jens Bissinger <mail@jens-bissinger.de>
2017-05-07 15:38:13 +0200

    Add runs from runtastic

--- fitness-tracker-web/src/config.js
+++ fitness-tracker-web/src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   target: {
     distance: 21.1, // km
-    time: 100 // min
+    time: 110 // min
   },
   mapboxToken: 'pk.eyJ1IjoiYnNpbmdyIiwiYSI6ImNqMmVjdDMyZzA1eWczM21wMGt3OG80Z3MifQ.XujhQ7LJuN5Rw9MtnVxSkA'
 }
