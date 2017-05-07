7b24115d695def28b1f6b63d5e845965befa8337
CI <bsingr@jens-col-2.fritz.box>
2017-05-07 21:03:59 +0200

    Fail early on geocode

--- src/reverseGeocode.js
+++ src/reverseGeocode.js
@@ -4,6 +4,9 @@ const cache = low('./geocode-cache.json');
 cache.defaults({urls: []}).write()
 
 const reverseGeocode = ({lat, lng}) => {
+  if (!lat || !lng) {
+    return Promise.reject(new Error(`Invalid coordinates lat=${lat} lng=${lng}`))
+  }
   const url = `http://nominatim.openstreetmap.org/reverse?format=json&lat=${lat.toFixed(3)}&lon=${lng.toFixed(3)}&zoom=18`
   const cachedData = cache.get('urls').find({url}).value();
   if (cachedData) {
