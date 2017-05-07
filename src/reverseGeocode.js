65cfe8d93cbc953180db6540301595b7617d2abd
CI <bsingr@jens-col-2.fritz.box>
2017-05-07 20:19:03 +0200

    Roughen geocode cache

--- src/reverseGeocode.js
+++ src/reverseGeocode.js
@@ -4,7 +4,7 @@ const cache = low('./geocode-cache.json');
 cache.defaults({urls: []}).write()
 
 const reverseGeocode = ({lat, lng}) => {
-  const url = `http://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lng}&zoom=18`;
+  const url = `http://nominatim.openstreetmap.org/reverse?format=json&lat=${lat.toFixed(3)}&lon=${lng.toFixed(3)}&zoom=18`
   const cachedData = cache.get('urls').find({url}).value();
   if (cachedData) {
     return Promise.resolve(cachedData.data);
