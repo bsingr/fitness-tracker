a30cf2773b94626f42e14bd58211ac37f940488f
CI <bsingr@jens-col-2.fritz.box>
2017-05-07 19:43:13 +0200

    Cache geocodes

--- src/reverseGeocode.js
+++ src/reverseGeocode.js
@@ -1,8 +1,21 @@
 const axios = require('axios');
+const low = require('lowdb');
+const cache = low('./geocode-cache.json');
+cache.defaults({urls: []}).write()
 
 const reverseGeocode = ({lat, lng}) => {
-  return axios.get(`http://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lng}&zoom=18`)
-  .then(res => res.data)
+  const url = `http://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lng}&zoom=18`;
+  const cachedData = cache.get('urls').find({url}).value();
+  if (cachedData) {
+    return Promise.resolve(cachedData.data);
+  } else {
+    return axios.get(url)
+    .then(res => res.data)
+    .then(data => {
+      cache.get('urls').push({url, data}).write();
+      return data;
+    })
+  }
 }
 
 module.exports = reverseGeocode;
